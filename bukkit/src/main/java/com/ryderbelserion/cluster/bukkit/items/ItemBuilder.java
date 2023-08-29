1d7cd972e4930172067f9de6f50b64678489f0ef
Ryder Belserion <no-reply@ryderbelserion.com>
2023-08-29 02:57:39 -0400

    man this sucks donkey dick

--- bukkit/src/main/java/com/ryderbelserion/cluster/bukkit/items/ItemBuilder.java
+++ bukkit/src/main/java/com/ryderbelserion/cluster/bukkit/items/ItemBuilder.java
@@ -3,13 +3,18 @@ package com.ryderbelserion.cluster.bukkit.items;
 import com.ryderbelserion.cluster.bukkit.BukkitPlugin;
 import com.ryderbelserion.cluster.bukkit.api.adventure.FancyLogger;
 import com.ryderbelserion.cluster.bukkit.api.registry.BukkitProvider;
+import net.minecraft.nbt.CompoundTag;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.craftbukkit.v1_20_R1.inventory.CraftItemStack;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import java.util.HashMap;
 
 public class ItemBuilder {
 
@@ -17,10 +22,12 @@ public class ItemBuilder {
 
     private final @NotNull JavaPlugin plugin = this.bukkitPlugin.getPlugin();
 
-    private final ItemStack itemStack;
-    private final ItemMeta itemMeta;
+    private ItemStack itemStack;
+    private ItemMeta itemMeta;
 
     private final Material material;
+
+    private String itemData;
 
     public ItemBuilder(ItemStack itemStack) {
         this.itemStack = itemStack;
@@ -31,16 +38,57 @@ public class ItemBuilder {
     }
 
     public ItemStack build() {
-        if (this.material != Material.AIR) {
-            this.itemMeta.getPersistentDataContainer().set(new NamespacedKey(this.plugin, ""), PersistentDataType.STRING, "");
+        if (!isAir()) {
+            if (!this.itemData.isEmpty()) {
+                net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(this.itemStack);
+
+                CompoundTag tag = new CompoundTag();
+
+                tag.putString("item-data", this.itemData);
+
+                nmsItem.setTag(tag);
+
+                ItemStack newStack = CraftItemStack.asBukkitCopy(nmsItem);
+
+                this.itemMeta = newStack.getItemMeta();
+                this.itemStack = newStack;
+
+                return this.itemStack;
+            }
         } else {
-            FancyLogger.warn("Material must be something other then AIR or not null.");
+            FancyLogger.warn("Material cannot be air.");
         }
 
         this.itemStack.setItemMeta(this.itemMeta);
 
-        this.itemMeta.getPersistentDataContainer().getKeys().stream().toList().forEach(key -> FancyLogger.debug(key.getKey()));
-
         return this.itemStack;
+    }
+
+    private boolean isAir() {
+        return this.material.isAir();
+    }
+
+    public ItemBuilder setItemData(String itemData) {
+        if (itemData.isBlank() || itemData.isEmpty()) return this;
+
+        this.itemData = itemData;
+
+        return this;
+    }
+
+    // Enchantments
+    public ItemBuilder addEnchantments(HashMap<Enchantment, Integer> enchantments, boolean unsafeEnchantments) {
+        enchantments.forEach((enchantment, level) -> addEnchantment(enchantment, level, unsafeEnchantments));
+        return this;
+    }
+
+    public ItemBuilder addEnchantment(Enchantment enchantment, int level, boolean unsafeEnchantments) {
+        this.itemMeta.addEnchant(enchantment, level, unsafeEnchantments);
+        return this;
+    }
+
+    public ItemBuilder removeEnchantment(Enchantment enchantment) {
+        this.itemMeta.removeEnchant(enchantment);
+        return this;
     }
 }
--- bukkit/src/main/java/com/ryderbelserion/cluster/plugin/commands/BuilderCommand.java
+++ bukkit/src/main/java/com/ryderbelserion/cluster/plugin/commands/BuilderCommand.java
@@ -2,16 +2,15 @@ package com.ryderbelserion.cluster.plugin.commands;
 
 import com.ryderbelserion.cluster.bukkit.items.ItemBuilder;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class BuilderCommand extends Command {
 
@@ -21,7 +20,9 @@ public class BuilderCommand extends Command {
 
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
-        String material = args[0];
+        String type = args[0];
+
+        String material = args[1];
 
         if (material.isEmpty() || material.isBlank()) return false;
 
@@ -31,6 +32,26 @@ public class BuilderCommand extends Command {
             ItemStack itemStack = new ItemStack(item);
 
             ItemBuilder builder = new ItemBuilder(itemStack);
+
+            switch (type) {
+                case "item-data" -> builder.setItemData("{Enchantments:[{id:\"minecraft:fire_aspect\",lvl:2s},{id:\"minecraft:sharpness\",lvl:5s},{id:\"minecraft:unbreaking\",lvl:3s}],Damage:0,slots:2,\"ae_enchantment;doublestrike\":1,\"ae_enchantment;disarmor\":1}");
+
+                case "enchants" -> {
+                    String enchant = args[2];
+
+                    if (!enchant.isBlank() || !enchant.isEmpty()) {
+                        Enchantment enchantment = Enchantment.getByKey(NamespacedKey.fromString(enchant));
+
+                        String level = args[3];
+
+                        if (level.isEmpty() || level.isBlank()) {
+                            if (enchantment != null) builder.addEnchantment(enchantment, 1, true);
+                        } else {
+                            if (enchantment != null) builder.addEnchantment(enchantment, Integer.parseInt(level), true);
+                        }
+                    }
+                }
+            }
 
             Player player = (Player) sender;
 
@@ -44,10 +65,48 @@ public class BuilderCommand extends Command {
 
     private static final List<String> ITEMS = Arrays.stream(Material.values()).filter(Material::isItem).map(Enum::name).toList();
 
+    private static List<String> getEnchants(ItemStack itemStack) {
+        return Arrays.stream(Enchantment.values()).filter(ench -> ench.canEnchantItem(itemStack)).map(key -> key.getKey().getKey()).toList();
+    }
+
+    private List<String> getNumbers() {
+        List<String> numbers = new ArrayList<>();
+
+        for (int value = 1; value <= 100; value += 1) numbers.add(String.valueOf(value));
+
+        return numbers;
+    }
+
     @Override
     public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
         if (args.length == 1) {
-            return StringUtil.copyPartialMatches(args[0].toLowerCase(), ITEMS, new ArrayList<>());
+            return StringUtil.copyPartialMatches(args[0].toLowerCase(), List.of(
+                    "enchants",
+                    "item-data",
+                    "lores"
+            ), new ArrayList<>());
+        }
+
+        if (args.length == 2) {
+            return StringUtil.copyPartialMatches(args[1].toLowerCase(), ITEMS, new ArrayList<>());
+        }
+
+        switch (args[0]) {
+            case "enchants" -> {
+                if (args.length == 3) {
+                    ItemStack itemStack = new ItemStack(Objects.requireNonNull(Material.matchMaterial(args[1])));
+
+                    return StringUtil.copyPartialMatches(args[2].toLowerCase(), getEnchants(itemStack), new ArrayList<>());
+                }
+
+                if (args.length == 4) {
+                    return StringUtil.copyPartialMatches(args[3], getNumbers(), new ArrayList<>());
+                }
+            }
+
+            case "other" -> {
+
+            }
         }
 
         return Collections.emptyList();
