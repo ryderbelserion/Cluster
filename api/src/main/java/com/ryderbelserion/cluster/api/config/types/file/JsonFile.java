78eb8cfa270b4fe1c60d92e8ecca0912ee70f41d
Ryder Belserion <no-reply@ryderbelserion.com>
2023-09-15 01:52:20 -0400

    Add json reader

--- api/src/main/java/com/ryderbelserion/cluster/api/config/types/file/JsonFile.java
+++ api/src/main/java/com/ryderbelserion/cluster/api/config/types/file/JsonFile.java
@@ -10,61 +10,35 @@ import java.lang.reflect.Modifier;
 
 public class JsonFile {
 
-    private final FileEngine context;
-
-    private final File file;
-    private final Gson gson;
+    private final JsonReader jsonReader;
 
     public JsonFile(FileEngine context) {
-        this.context = context;
+        File file = context.getFile();
 
-        this.file = this.context.getFile();
-
-        if (this.context.getGson() != null) {
-            this.gson = this.context.getGson().create();
-            return;
-        }
-
-        GsonBuilder builder = new GsonBuilder().disableHtmlEscaping()
+        GsonBuilder builder = new GsonBuilder()
+                .disableHtmlEscaping()
                 .enableComplexMapKeySerialization()
                 .excludeFieldsWithModifiers(Modifier.TRANSIENT)
                 .excludeFieldsWithoutExposeAnnotation();
 
-        this.gson = builder.create();
+        Gson gson = (context.getGson() != null) ? context.getGson().create() : builder.create();
+
+        try {
+            if (file.createNewFile()) {
+                FancyLogger.debug("Created new file: " + file.getName());
+            }
+        } catch (IOException exception) {
+            FancyLogger.error("Failed to create " + file.getName(), exception);
+        }
+
+        this.jsonReader = new JsonReader(file, gson, context);
     }
 
     public void load() {
-        try {
-            if (this.file.createNewFile()) FancyLogger.debug("Created new file: " + this.file.getName());
-        } catch (Exception exception) {
-            FancyLogger.error("Failed to create " + this.file.getName());
-            FancyLogger.debug("Reason: " + exception.getMessage());
-        }
-
-        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(this.file), StandardCharsets.UTF_8)) {
-            this.gson.fromJson(reader, this.context.getClass());
-        } catch (Exception exception) {
-            FancyLogger.error("Failed to load " + this.file.getName());
-            FancyLogger.debug("Reason: " + exception.getMessage());
-        }
+        this.jsonReader.readFile();
     }
 
     public void save() {
-        try {
-            if (this.file.createNewFile()) FancyLogger.debug("Created new file: " + this.file.getName());
-
-            write();
-        } catch (Exception exception) {
-            FancyLogger.error("Failed to write/save to " + this.file.getName());
-            FancyLogger.debug("Reason: " + exception.getMessage());
-        }
-    }
-
-    private void write() throws IOException {
-        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(this.file), StandardCharsets.UTF_8)) {
-            String values = this.gson.toJson(this.context, this.context.getClass());
-
-            writer.write(values);
-        }
+        this.jsonReader.saveFile();
     }
 }
---
+++ api/src/main/java/com/ryderbelserion/cluster/api/config/types/file/JsonReader.java
@@ -0,0 +1,46 @@
+package com.ryderbelserion.cluster.api.config.types.file;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.ryderbelserion.cluster.api.adventure.FancyLogger;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+public class JsonReader {
+
+    private final File file;
+    private final Gson gson;
+    private final Object context;
+
+    public JsonReader(File file, Gson gson, Object context) {
+        this.file = file;
+        this.gson = gson;
+        this.context = context;
+    }
+
+    public void readFile() {
+        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(this.file), StandardCharsets.UTF_8)) {
+            this.gson.fromJson(reader, this.context.getClass());
+        } catch (IOException exception) {
+            FancyLogger.error("Failed to read " + this.file.getName(), exception);
+        } catch (JsonSyntaxException exception) {
+            FancyLogger.error("Failed to parse json from " + this.file.getName(), exception);
+        }
+    }
+
+    public void saveFile() {
+        try {
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(this.file), StandardCharsets.UTF_8)) {
+                String values = this.gson.toJson(this.context, this.context.getClass());
+                writer.write(values);
+            }
+        } catch (IOException exception) {
+            FancyLogger.error("Failed to write to " + this.file.getName(), exception);
+        }
+    }
+}
