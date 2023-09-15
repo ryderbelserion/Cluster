ec511153aaa9e1a5cf7a93ff05685ea3064fba1f
Ryder Belserion <no-reply@ryderbelserion.com>
2023-09-15 01:51:21 -0400

    Add extra loggers

--- api/src/main/java/com/ryderbelserion/cluster/api/adventure/FancyLogger.java
+++ api/src/main/java/com/ryderbelserion/cluster/api/adventure/FancyLogger.java
@@ -8,7 +8,7 @@ public class FancyLogger {
     private static String name = "";
 
     public static void setName(String prefix) {
-        if (!name.isBlank() || prefix.isBlank()) return;
+        if (prefix.isBlank()) return;
 
         name = prefix;
     }
@@ -17,20 +17,40 @@ public class FancyLogger {
         send(name + " <blue>INFO</blue> " + message);
     }
 
+    public static void info(String message, Throwable cause) {
+        info(message + ". Reason: " + cause.getMessage());
+    }
+
     public static void warn(String message) {
         send(name + " <gold>WARNING</gold> " + message);
+    }
+
+    public static void warn(String message, Throwable cause) {
+        warn(message + ". Reason: " + cause.getMessage());
     }
 
     public static void error(String message) {
         send(name + " <red>ERROR</red> " + message);
     }
 
+    public static void error(String message, Throwable cause) {
+        error(message + ". Reason: " + cause.getMessage());
+    }
+
     public static void debug(String message) {
         send(name + " <yellow>DEBUG</yellow> " + message);
     }
 
+    public static void debug(String message, Throwable cause) {
+        debug(message + ". Reason: " + cause.getMessage());
+    }
+
     public static void success(String message) {
         send(name + " <green>SUCCESS</green> " + message);
+    }
+
+    public static void success(String message, Throwable cause) {
+        success(message + ". Reason: " + cause.getMessage());
     }
 
     private static void send(String message) {
