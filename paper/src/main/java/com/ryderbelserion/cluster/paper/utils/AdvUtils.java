fc744c7cd6b2b854d3b2068dacfa8e234a2f96e0
Ryder Belserion <no-reply@ryderbelserion.com>
2023-12-02 13:29:49 -0500

    update parse message

--- paper/src/main/java/com/ryderbelserion/cluster/paper/utils/AdvUtils.java
+++ paper/src/main/java/com/ryderbelserion/cluster/paper/utils/AdvUtils.java
@@ -7,10 +7,6 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 public class AdvUtils {
 
     public static Component parse(String message) {
-        return MiniMessage.miniMessage().deserialize(message);
-    }
-
-    public static Component parse(String message, TextDecoration.State state) {
-        return parse(message).decorationIfAbsent(TextDecoration.ITALIC, state);
+        return MiniMessage.miniMessage().deserialize(message).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE);
     }
 }
