a9287f584f7e8a5f1a1d744ff4df9b44e1b37e2e
Ryder Belserion <no-reply@ryderbelserion.com>
2024-01-11 23:17:49 -0500

    add text color method

--- paper/src/main/java/com/ryderbelserion/cluster/utils/DyeUtils.java
+++ paper/src/main/java/com/ryderbelserion/cluster/utils/DyeUtils.java
@@ -1,5 +1,6 @@
 package com.ryderbelserion.cluster.utils;
 
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Color;
 import org.bukkit.DyeColor;
 import java.util.HashMap;
@@ -46,6 +47,23 @@ public class DyeUtils {
             int green = Integer.parseInt(rgb[1]);
             int blue = Integer.parseInt(rgb[2]);
             return Color.fromRGB(red, green, blue);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException ignore) {}
+
+        return null;
+    }
+
+    public static TextColor getTextColor(String color) {
+        if (color == null || color.isBlank()) return null;
+
+        try {
+            String[] rgb = color.split(",");
+
+            if (rgb.length != 3) return null;
+
+            int red = Integer.parseInt(rgb[0]);
+            int green = Integer.parseInt(rgb[1]);
+            int blue = Integer.parseInt(rgb[2]);
+            return TextColor.color(red, green, blue);
         } catch (NumberFormatException | ArrayIndexOutOfBoundsException ignore) {}
 
         return null;
