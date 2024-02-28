5cc4694de08e0375aef4cfc579617f47dde124a8
Ryder Belserion <no-reply@ryderbelserion.com>
2024-02-27 23:00:22 -0500

    Simplify component builder

--- api/src/main/java/com/ryderbelserion/cluster/api/builders/ComponentBuilder.java
+++ api/src/main/java/com/ryderbelserion/cluster/api/builders/ComponentBuilder.java
@@ -1,6 +1,7 @@
 package com.ryderbelserion.cluster.api.builders;
 
 import com.ryderbelserion.cluster.utils.AdvUtils;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
@@ -11,51 +12,88 @@ public class ComponentBuilder {
 
     private final TextComponent.@NotNull Builder builder = Component.text();
 
-    private final FancyComponentBuilder fancyComponentBuilder;
+    private final Audience target;
 
-    public ComponentBuilder() {
-        this.fancyComponentBuilder = new FancyComponentBuilder();
+    public ComponentBuilder(Audience target) {
+        this.target = target;
     }
 
-    private String message;
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
+    /**
+     * Appends a new component to the message.
+     *
+     * @param component the component to append.
+     * @return the Builder object with updated information.
+     */
     public ComponentBuilder append(Component component) {
         this.builder.append(component);
 
         return this;
     }
 
-    public Component parse(String value) {
-        return AdvUtils.parse(value);
-    }
-
+    /**
+     * Appends a new component to the message.
+     *
+     * @param text the text shown on hover.
+     * @return the Builder object with updated information.
+     */
     public ComponentBuilder hover(String text) {
-        this.builder.hoverEvent(HoverEvent.showText(parse(text)));
+        this.builder.hoverEvent(HoverEvent.showText(AdvUtils.parse(text)));
 
         return this;
     }
 
+    /**
+     * Adds a click event to the message.
+     *
+     * @param action the click action to use.
+     * @param text the text that is sent on click.
+     * @return the Builder object with updated information.
+     */
     public ComponentBuilder click(ClickEvent.Action action, String text) {
         this.builder.clickEvent(ClickEvent.clickEvent(action, text));
 
         return this;
     }
 
-    public FancyComponentBuilder getFancyComponentBuilder() {
-        return this.fancyComponentBuilder;
-    }
-
-    public @NotNull TextComponent build() {
-        Component message = parse(this.message);
-
-        if (this.fancyComponentBuilder.fancy() != null) {
-            return this.builder.append(message).append(this.fancyComponentBuilder.fancy()).build();
-        }
+    /**
+     * @return the final component.
+     */
+    @NotNull
+    public TextComponent getComponent() {
+        Component message = AdvUtils.parse(this.value);
 
         return this.builder.append(message).build();
+    }
+
+    /**
+     * Send a component to someone.
+     */
+    public void send() {
+        this.target.sendMessage(getComponent());
+    }
+
+    /**
+     * @return get the target recipient.
+     */
+    public Audience getTarget() {
+        return this.target;
+    }
+
+    private String value;
+
+    /**
+     * Sets the message to the value.
+     *
+     * @param value the string to use.
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the message.
+     */
+    public String getValue() {
+        return this.value;
     }
 }
