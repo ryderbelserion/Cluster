a3235d9f980e83c343ff071f788d3406d3fc26d1
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-05 22:28:58 -0500

    organize version catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,22 +17,18 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("libs") {
-            version("adventure4", "4.14.0")
+            version("adventure4", "4.16.0")
 
             library("adventure4", "net.kyori", "adventure-text-minimessage").versionRef("adventure4")
-
-            library("itemsadder", "com.github.LoneDev6", "api-itemsadder").version("3.6.1")
-
             library("minimessage4", "net.kyori", "adventure-api").versionRef("adventure4")
 
-            library("placeholderapi", "me.clip", "placeholderapi").version("2.11.5")
-
-            library("arcaniax", "com.arcaniax", "HeadDatabase-API").version("1.3.0")
-
+            library("itemsadder", "com.github.LoneDev6", "api-itemsadder").version("3.6.1")
             library("oraxen", "io.th0rgal", "oraxen").version("1.164.0")
 
-            plugin("paperweight", "io.papermc.paperweight.userdev").version("1.5.9")
+            library("placeholderapi", "me.clip", "placeholderapi").version("2.11.5")
+            library("arcaniax", "com.arcaniax", "HeadDatabase-API").version("1.3.0")
 
+            plugin("paperweight", "io.papermc.paperweight.userdev").version("1.5.9")
             plugin("shadow", "com.github.johnrengelman.shadow").version("8.1.1")
 
             bundle("adventure", listOf("adventure4", "minimessage4"))
