eace63372f478e8782c6481630be2587f9a1a6a6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-24 16:16:01 -0400

    Update build script

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,10 +9,10 @@ plugins {
 dependencies {
     paperweight.paperDevBundle(libs.versions.bundle)
 
-    compileOnlyApi(libs.itemsadder.api)
-    compileOnlyApi(libs.oraxen.api)
-    compileOnlyApi(libs.placeholder.api)
-    compileOnlyApi(libs.head.database.api)
+    compileOnly(libs.itemsadder.api)
+    compileOnly(libs.oraxen.api)
+    compileOnly(libs.placeholder.api)
+    compileOnly(libs.head.database.api)
 
     api(libs.simple.yaml)
 
